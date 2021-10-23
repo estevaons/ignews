d726b58878c41907e65582c7dc664a9e3a1a08a1
Estevão Nunes <estevaons.gt@gmail.com>
2021-10-23 15:23:56 -0300

    integração com stripe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,24 +2,29 @@ import styles from './styles.module.scss'
 import { FiX } from 'react-icons/fi';
 import { FaGithub } from 'react-icons/fa'
 
+import { signIn,signOut, useSession } from 'next-auth/client'
+
 export function SignInButton(){
+    const [session] = useSession()
 
-    const isUserLoggedIn = true;
+    console.log(session)
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={()=>signOut()}
         
         >
             <FaGithub color="#04d361"/>
-            Estevão Nunes
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ):(
         <button
             type="button"
             className={styles.signInButton}
+            onClick={()=> signIn('github')}
         
         >
             <FaGithub color="#eba417"/>
